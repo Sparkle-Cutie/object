855603d3fab8ff4041b419c5802fda2ff8be9cbe
Sparkle-Cutie <103934686+Sparkle-Cutie@users.noreply.github.com>
2022-05-09 15:28:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+image = "";
+function preload(){
+    img = loadImage("lalala.jpg");
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Tiger", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+}
